57c2a77c75713763d12e2c244a15658f94c4823a
Eduardo Dallmann <eduardo.dallmann@ambevtech.com.br>
2022-06-12 15:40:19 -0300

    ajustes dash

--- src/screens/home-screen.tsx
+++ src/screens/home-screen.tsx
@@ -1,22 +1,25 @@
 import React from 'react';
 import {Description} from '../components/description';
 import {Panel} from '../components/panel';
+import {CenterDash} from './styles';
 
 export function HomeScreen() {
   return (
     <>
       <Panel title="Leitura fora da tolerância nas últimas 24 horas">
-        <Description
-          title="20,65°C"
-          subtitle="00:15:01 última leitura fora da tolerância"
-          error
-        >
-          O equipamento Geladeira Fundos fez 1 leitura fora da tolerância
-        </Description>
+        <CenterDash>
+          <Description
+            title="20,65°C"
+            subtitle="00:15:01 última leitura fora da tolerância"
+            error
+          >
+            O equipamento Geladeira Fundos fez 1 leitura fora da tolerância
+          </Description>
+        </CenterDash>
       </Panel>
 
       <Panel title="Status atual dos equipamentos">
-        <div style={{display: 'flex', flexWrap: 'wrap', gap: '20px'}}>
+        <CenterDash>
           <Description title="14,32°C" subtitle="00:25:01 última leitura">
             O equipamento Geladeira Fundos está com a temperatura dentro do
             esperado
@@ -45,7 +48,7 @@ export function HomeScreen() {
             O equipamento Geladeira Pública está com a temperatura dentro do
             esperado
           </Description>
-        </div>
+        </CenterDash>
       </Panel>
     </>
   );
---
+++ src/screens/styles.ts
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+export const CenterDash = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+
+  @media (max-width: 753px) {
+    justify-content: center;
+  }
+`;
